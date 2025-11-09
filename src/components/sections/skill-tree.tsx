bdbd8a7ee16c522dadbc1ef7fc4a898eef510a5a
peter <2814594602@qq.com>
2025-11-09 23:41:15 +0800

    feat: input component success

---
+++ src/components/sections/skill-tree.tsx
@@ -0,0 +1,15 @@
+"use client ";
+
+const GsapSkillsTree = () => {
+  const Skills = [
+    // Web skill
+    { name: "React", level: 95, group: "web", color: "#61DAFB" },
+    { name: "Next.js", level: 90, group: "web", color: "#000000" },
+    { name: "Vue.js", level: 85, group: "web", color: "#4FC08D" },
+    { name: "TypeScript", level: 90, group: "web", color: "#3178C6" },
+    { name: "JavaScript", level: 95, group: "web", color: "#F7DF1E" },
+    { name: "HTML/CSS", level: 95, group: "web", color: "#E34F26" },
+  ];
+};
+
+export default GsapSkillsTree;
